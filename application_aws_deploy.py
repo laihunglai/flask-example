d16f502d2da1025600ad19a55e68d41c2411f16d
laihunglai <99182091+laihunglai@users.noreply.github.com>
2022-02-07 22:42:02 +0800

    Update application_aws_deploy.py

--- application_aws_deploy.py
+++ application_aws_deploy.py
@@ -1,5 +1,5 @@
 from flask import Flask
-application = Flask(__name__)
-@application.route('/')
+application_aws_deploy = Flask(__name__)
+@application_aws_deploy.route('/')
 def hello_world():
     return 'Hello world.'
